161e1ede2795713bb67fe4da7ba343a2c1322721
Maximus <tomiwadairo@gmail.com>
2024-08-17 17:45:30 +0100

    added a search bar

---
+++ app/components/SearchInstallation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchInstallation = () => {
+  return (
+      <form>
+          <input className='form-control border text-center rounded-full p-[10px]  mt-[24px]' type='text' id='search' name='search' placeholder='Search Customer'/>
+    </form>
+  )
+}
+
+export default SearchInstallation
