c30ce18105423064475d277e7c10713e7af664f6
Maximus <tomiwadairo@gmail.com>
2024-08-17 18:09:14 +0100

    added search button to search

--- app/components/SearchInstallation.tsx
+++ app/components/SearchInstallation.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const SearchInstallation = () => {
   return (
-      <form>
-          <input className='form-control border text-center rounded-full p-[10px]  mt-[24px]' type='text' id='search' name='search' placeholder='Search Customer'/>
+    <form className='border flex flex-row  w-fit justify-between h-full relative rounded-full '>
+          <input className=' border text-cente rounded-full p-[10px]' type='text' id='search' name='search' placeholder='Search Customer' />
+      <button className="px-4 py-2 bg-indigo-600 text-xl absolute right-[4px] top-[8%] text-white rounded-full  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 border-white shadow">Find</button>
     </form>
   )
 }
